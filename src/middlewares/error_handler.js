1c3651f38937df393f66ddab9e9292466951c624
antoine <antoine.gayoso@gmail.com>
2021-07-06 13:40:51 +0200

    feat: middlewares errors added

---
+++ src/middlewares/error_handler.js
@@ -0,0 +1,25 @@
+const { SERVER_ERROR } = require("../helpers/status_codes");
+
+module.exports = (error, request, response, next) => {
+  const { errors } = error;
+  let { status, message, description } = error;
+
+  if (!status) {
+    status = SERVER_ERROR;
+    message = "Oups ! Quelque chose ne fonctionne pas !";
+    description =
+      "Le serveur rencontre un problème technique. Veuillez réessayer plus tard.";
+  }
+
+  if (error.name === "ValidationError") {
+    response.status(status).json({
+      message,
+      errors,
+    });
+  } else {
+    response.status(status).json({
+      message,
+      description,
+    });
+  }
+};
