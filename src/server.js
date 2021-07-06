066d8453c1e9447e9b9933f06fdcb5478cb9114b
antoine <antoine.gayoso@gmail.com>
2021-07-06 13:53:51 +0200

    feat: server configured and index done

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const helmet = require("helmet");
+const logger = require("morgan");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const { notFoundHandler, errorLogger, errorHandler } = require("./middlewares");
+
+const server = express();
+
+server.use(helmet());
+server.use(logger("dev"));
+server.use(express.json());
+
+server.use("/api", cors());
+
+server.use("/api", routes);
+
+server.use("*", notFoundHandler);
+server.use(errorLogger);
+server.use(errorHandler);
+
+module.exports = server;
