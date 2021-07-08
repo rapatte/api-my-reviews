3330083f3207dced9ff89bc00c3a475e84b9129e
antoine <antoine.gayoso@gmail.com>
2021-07-08 16:52:22 +0200

    feat: admin and genre routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 require("express-async-errors");
 
 const reviewsRouter = require("./reviews_router");
+const adminsRouter = require("./admins_router");
+const genresRouter = require("./genres_router");
 
 const mainRouter = express.Router();
 
 mainRouter.use("/reviews", reviewsRouter);
+mainRouter.use("/admins", adminsRouter);
+mainRouter.use("/genres", genresRouter);
 
 module.exports = mainRouter;
